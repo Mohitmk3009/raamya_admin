75dd38104ec453b3113e9c299cf61e92ebae2390
Mohit Kumar <96984074+Mohitmk3009@users.noreply.github.com>
2025-09-24 01:22:32 +0530

    changes layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   description: "Manage your Raamya store",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="antialiased bg-black">
